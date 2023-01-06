6758ab009bd1f7a75737dd7a7f2e6b0360ede440
Matteo Giuntoni <59317264+Matteo181202@users.noreply.github.com>
2023-01-06 20:57:44 +0100

    Init map setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,24 @@
 #include "Game.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
 
 int main(void){
+    FILE* file = fopen("map.txt", "r");
+    if (file == nullptr) {
+        std::cout << "Errore" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    unsigned char character = 0;
+
+    while (!feof(file)){
+        character = getc(file);
+        std::cout << character << "-";
+    }
+    std::cout << std::endl;
+    fclose(file);
+
     Game game;
     bool success = game.initialize();
     if (success) game.runLoop();
